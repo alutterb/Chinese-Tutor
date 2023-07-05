abc3a6a0827a119d6520a3b853730704cc46eb16
akagi <rmhslutterbach@gmail.com>
2023-07-05 18:58:21 -0500

    using gpt 3.5 for correction of extracted text

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,12 @@
 import os
 import pdfplumber
 from dotenv import load_dotenv
-import pygtrie
-import re
+import openai
 
+# load environment variables
 load_dotenv()
-
-# ======================= ENGLISH DICTIONARY AND PINYIN DATASET ============
-PINYIN_PATH = os.getenv('PINYIN_PATH')
-
-# Load pinyin dataset
-def load_pinyin_set():
-    pinyin = set()
-    with open(PINYIN_PATH) as file:
-        for word in file:
-            word = word.strip()
-            pinyin.add(word)
-    return pinyin
-
-pinyin_set = load_pinyin_set()
+# Hyperparameters
+MAX_TOKENS = 2000
 
 # =======================  TEXT EXTRACTION ===================== #
 '''
@@ -26,58 +14,79 @@ Converts the textbook pdf into images, and then uses an ocr scanner
 to extract the text
 '''
 
+# Paths to pdf input and text output
 TEXTBOOK_PDF_PATH = os.getenv('TEXTBOOK_PDF_PATH')
 TEXTBOOK_TXT_PATH = os.getenv('TEXTBOOK_TXT_PATH')
 
-# Check if a string contains pinyin characters
-def contains_pinyin(word):
-    return bool(re.match(r'[a-zA-Z]*[āēīōūǖĀĒĪŌŪǕáéíóúǘÁÉÍÓÚǗǎěǐǒǔǚǍĚǏǑǓǙàèìòùǜÀÈÌÒÙǛ][a-zA-Z]*', word))
+# load openai api key
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Create trie structure based on pinyin data
-def load_pinyin_trie():
-    trie = pygtrie.CharTrie()
-    for pinyin in pinyin_set:
-        trie[pinyin] = pinyin
-    return trie
+# Truncates raw text based on MAX_TOKENS size and returns chunks of text 
+# where each chunk is less than MAX_TOKENS
+def truncate_text(raw_text):
+    text_len = len(raw_text)
+    remaining = text_len - MAX_TOKENS
+    if remaining <= 0:
+        return [raw_text]
+    pointer = 0
+    chunks = []
+    while remaining > 0:
+        lower = pointer
+        upper = min(text_len, pointer + MAX_TOKENS)
+        chunk = raw_text[lower:upper]
+        chunks.append(chunk)
+        new_text_len = len(raw_text)
+        remaining = text_len - new_text_len
+        pointer += MAX_TOKENS
+    return chunks
 
-# Checks word in trie with longest matching prefix with the supplied pinyin word
-def pinyin_match_trie(pinyin, correct_pinyins_trie):
-    match = correct_pinyins_trie.longest_prefix(pinyin)
-    if match:
-        return match.value
-    else:
-        return pinyin
-
-
-# Proccesses pinyin in each page, attempts to fix missing tones from extracted text
-def process_pinyin(input_text, correct_pinyins_trie):
-    words = input_text.split()
-    processed_words = []
-    for word in words:
-        if contains_pinyin(word): 
-            word = pinyin_match_trie(word, correct_pinyins_trie)
-        processed_words.append(word)
-    return ' '.join(processed_words)
+# Call openai api to correct spelling, grammar, punctuation, and add tone markers to
+# to pinyin 
+def correct_text(text):
+    system_intel = '''
+    You are a chinese and linguistic expert. Fix the following broken text into a cohesive sentence. 
+    Be sure to also add tones to pinyin with missing tones:\n'''
+    corrected_text = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role" : "system", "content" : system_intel},
+            {"role" : "user", "content" : text}
+        ],
+        temperature=0,
+        max_tokens=MAX_TOKENS,
+        top_p=1,
+        frequency_penalty=0.5,
+        presence_penalty=0.5
+    )
+    return corrected_text
 
 # Converts pdf to text
-def extract_text_from_pdf(pdf_path):
+def extract_and_process_text_from_pdf(pdf_path):
     all_text = ''
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
-            text = page.extract_text()
-            all_text += text
+            # extract raw text from pdf
+            raw_text = page.extract_text()
+            # break up input into chunks up to MAX_TOKENS
+            raw_chunks = truncate_text(raw_text)
+            # loop through chunks and apply openai api to correct text
+            for chunk in raw_chunks:
+                corrected_text = correct_text(chunk)
+                # acquire text from openai result
+                corrected_text = corrected_text['choices'][0]['message']['content']
+                all_text += corrected_text
     return all_text
 
 # Do everything
-def extract_and_process_text_from_pdf():
-    correct_pinyins = load_pinyin_trie()
+def write_pdf_to_text():
     try:
         if not os.path.exists(TEXTBOOK_PDF_PATH):
             print(f"File not found: {TEXTBOOK_PDF_PATH}")
             return
-        raw_text = extract_text_from_pdf(TEXTBOOK_PDF_PATH)
-        # clean pinyin from raw text
-        all_text = process_pinyin(raw_text, correct_pinyins)
+        # extract text from pdf and process it
+        print("Extracting and correcting text...")
+        all_text = extract_and_process_text_from_pdf(TEXTBOOK_PDF_PATH)
+
         # save all text to a txt file.
         with open(TEXTBOOK_TXT_PATH, 'w', encoding='utf-8') as f:
             f.write(all_text)
@@ -88,8 +97,6 @@ def extract_and_process_text_from_pdf():
         print(f"File not found: {TEXTBOOK_PDF_PATH}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
-extract_and_process_text_from_pdf()
 # ========================================================================
 
     
